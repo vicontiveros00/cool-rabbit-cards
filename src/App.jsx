ca69c85ba54f61e6558a1bc1e43c059b2d117e2f
Vic <vontiverosewhs@gmail.com>
2022-11-09 23:19:39 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+import Cards from './Components/Cards';
+import React from 'react';
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Cards />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Cards.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Cards.css';
+
+class Cards extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="card-group">
+                    <div className="little-card card">
+                    </div>
+                    <div className="big-card card">
+                    </div>
+                    <div className="little-card card">
+                    </div>
+                    <div className="big-card card">
+                    </div>
+                    <div className="little-card card">
+                    </div>
+                    <div className="big-card card">
+                    </div>
+                    <div className="little-card card">
+                    </div>
+                    <div className="big-card card">
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Cards;
